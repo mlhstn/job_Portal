6a240ea0ef918388219e9bf9bc064dd3ea6d262c
melih üstün <melih.stn98@gmail.com>
2024-09-09 22:34:50 +0300

    finish

---
+++ src/main/java/com/luv2code/jobportal/entity/Users.java
@@ -0,0 +1,106 @@
+package com.luv2code.jobportal.entity;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "users")
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int userId;
+
+    @Column(unique = true)
+    private String email;
+
+    @NotEmpty
+    private String password;
+
+    private boolean isActive;
+
+    @DateTimeFormat(pattern = "dd-MM-yyyy")
+    private Date registrationDate;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "userTypeId", referencedColumnName =  "userTypeId")
+    private UsersType userTypeId;
+
+    public Users() {
+
+    }
+
+    public Users(int userId, String email, String password, boolean isActive, Date registrationalDate, UsersType userTypeId) {
+        this.userId = userId;
+        this.email = email;
+        this.password = password;
+        this.isActive = isActive;
+        this.registrationDate = registrationalDate;
+        this.userTypeId = userTypeId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public Date getRegistrationalDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationalDate(Date registrationalDate) {
+        this.registrationDate = registrationalDate;
+    }
+
+    public UsersType getUserTypeId() {
+        return userTypeId;
+    }
+
+    public void setUserTypeId(UsersType userTypeId) {
+        this.userTypeId = userTypeId;
+    }
+
+    @Override
+    public String toString() {
+
+        return "Users{" +
+                "userId=" + userId +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", isActive=" + isActive +
+                ", registrationalDate=" + registrationDate +
+                ", userTypeId=" + userTypeId +
+                '}';
+    }
+}
